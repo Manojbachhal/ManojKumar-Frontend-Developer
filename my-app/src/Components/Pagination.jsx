722be0d172b51a5800badfbadaeac6216e8775ab
manoj kumar <manojtewatia9211@gmail.com>
2023-05-31 17:02:36 +0530

    Buttons and popup redesigned

--- my-app/src/Components/Pagination.jsx
+++ my-app/src/Components/Pagination.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Pagination({ page, setPage }) {
     return (
-        <div className='text-center'>
+        <div className='text-center pb-10'>
 
 
-            <button class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            <button class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-teal-600 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 disabled={page == 0}
                 onClick={() => {
                     setPage(page - 1)
@@ -14,7 +14,7 @@ function Pagination({ page, setPage }) {
             </button>
 
 
-            <button class="inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            <button class="inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-500 bg-white border border-teal-600 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 onClick={() => {
                     setPage(page + 1)
                 }}>
--- my-app/src/Components/Popup.jsx
+++ my-app/src/Components/Popup.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
+import React from "react";
 
 function Popup({ popup, setPopup }) {
-    console.log(popup.data)
+    console.log(popup.data);
     return (
-        <div style={{ display: popup.vibility ? 'block' : 'none' }} className=' m-auto fixed bg-red-200 text-black inset-0 top-20 p-10 w-5/6 h-3/4 rounded-lg'>
-            <div className='flex justify-end'>
-                <button className='rounded-lg bg-red-600 text-base  text-white w-10 h-10 align-left rounded-full' onClick={() => setPopup({ ...popup, vibility: false })}>X</button>
+        <div
+            id="popup"
+            style={{ display: popup.vibility ? "block" : "none" }}
+            className=" m-auto fixed bg-white text-black inset-0 top-20 p-10 w-5/6 h-3/4 rounded-lg"
+        >
+            <div className="flex justify-end">
+                <button
+                    className="bg-teal-600 text-base  text-white w-10 h-10 align-left rounded-full"
+                    onClick={() => setPopup({ ...popup, vibility: false })}
+                >
+                    X
+                </button>
             </div>
             <div>
-
-                <h1 className="text-xl" style={{ color: '#F55C30' }}>Capsule Name : {popup.data.capsule_id}</h1>
-                <h2>details : {popup.data.details}</h2>
+                <h1 className="text-xl text-teal-600">
+                    Capsule Name : {popup.data.capsule_id}
+                </h1>
+                <h2>Details : {popup.data.details}</h2>
                 <p>Serial Number : {popup.data.capsule_serial}</p>
                 <p>Status : {popup.data.status}</p>
                 <p>Type : {popup.data.type}</p>
                 <p>landings : {popup.data.landings}</p>
-                <p>Launch : {popup.data.original_launch == null ? "Null" : popup.data.original_launch}</p>
+                <p>
+                    Launch :{" "}
+                    {popup.data.original_launch == null
+                        ? "Null"
+                        : popup.data.original_launch}
+                </p>
             </div>
         </div>
-    )
+    );
 }
 
-export default Popup
+export default Popup;
